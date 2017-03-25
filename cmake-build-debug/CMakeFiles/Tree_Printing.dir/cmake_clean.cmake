3c0024f0a6ee2deb0bc7f649501a2458e1861ed8
Shubham Gupta <shubham@gupta.net.in>
2017-03-24 23:59:03 -0400

    Creating New project (old got deleted, mistake)

---
+++ cmake-build-debug/CMakeFiles/Tree_Printing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tree_Printing.dir/main.cpp.obj"
+  "Tree_Printing.pdb"
+  "Tree_Printing.exe"
+  "Tree_Printing.exe.manifest"
+  "libTree_Printing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tree_Printing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
