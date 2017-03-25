ee0ec226379ef19da53f4a836fd67533da20ca41
Shubham Gupta <shubham@gupta.net.in>
2017-03-25 00:26:46 -0400

    Creating nodes
    Creating nodes; namespace tree; starting tree

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <utility>
+#include <algorithm>
+#include <list>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+namespace tree {
+
+    template<typename T>
+    struct node
+    {
+        T data;
+        node* l;
+        node* r;
+        node(T&& data_ = T()) : data(std::move(data_)), l(0), r(0) {}
+    };
+
+}
+
+int main()
+{
+    typedef tree::node<int> node;
+
+    node* head = new node(1);
+
+    head->l    = new node(1);
+    head->r    = new node(1);
+
     return 0;
 }
